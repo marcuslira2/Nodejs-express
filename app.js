e0649775eb01570b75037c7cf2c0dbd400997ced
Marcus Lira <marcuslira57@gmail.com>
2022-02-07 06:02:39 -0300

    update

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
 const port = 3000
-const {randomUUID} = require("crypto")
+const { randomUUID } = require("crypto")
 
 
 app.use(express.json());
 
 const products = [];
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.json({
         message: 'primeira rota',
         data: 'messagem aleatoria'
     })
 })
 
-app.post("/products",(req, res)=>{
-    const {name, price} = req.body;
+app.post("/products", (req, res) => {
+    const { name, price } = req.body;
 
     const product = {
         name,
@@ -29,16 +29,39 @@ app.post("/products",(req, res)=>{
     return res.json(product)
 });
 
-app.get("/products", (req,res)=>{
+app.get("/products", (req, res) => {
     return res.json(products)
-})
+});
 
-app.get("/products/:id",(req,res)=>{
-    const {id} = req.params;
-    const product = products.find( product => product.id === id);
+app.get("/products/:id", (req, res) => {
+    const { id } = req.params;
+    const product = products.find(product => product.id === id);
     return res.json(product);
-})
+});
 
-app.listen(port, ()=> {
-    console.log('servidor esta rodando na porta',port)
+app.put("/products/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, price } = req.body;
+    const productIndex = products.findIndex(product => product.id === id);
+    products[productIndex] = {
+        ...products[productIndex],
+        name,
+        price
+    };
+
+    return res.json({ message: "Produto alterado com sucesso!" });
+});
+
+
+app.delete("/products/:id", (req, res) => {
+    const { id } = req.params;
+    const productIndex = products.findIndex(product => product.id === id);
+
+    products.splice(productIndex, 1);
+
+    return res.json({message: "Produto removido com sucesso"})
+});
+
+app.listen(port, () => {
+    console.log('servidor esta rodando na porta', port)
 });
